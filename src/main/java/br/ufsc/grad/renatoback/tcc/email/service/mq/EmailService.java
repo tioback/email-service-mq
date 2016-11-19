f7c42202f2558dcf305c0ecbeeee99598beed6bd
renato <renato@knewin.com>
2016-11-19 18:36:17 -0200

    Limpeza.

--- src/main/java/br/ufsc/grad/renatoback/tcc/email/service/mq/EmailService.java
+++ src/main/java/br/ufsc/grad/renatoback/tcc/email/service/mq/EmailService.java
@@ -1,21 +1,11 @@
 package br.ufsc.grad.renatoback.tcc.email.service.mq;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-	private Log logger = LogFactory.getLog(EmailService.class);
-
-	AtomicInteger counter = new AtomicInteger();
-
-	public void sendEmail(String time) {
-		logger.info(String.format("OK Email #%d after %d ns", counter.incrementAndGet(),
-				System.nanoTime() - Long.parseLong(time)));
+	public void sendEmail(String userId) {
+		System.err.println(".");
 	}
-
 }
